94a7c428ea7dcc7b777814d4fd8b0a7858148c13
Somto Ufondu <somtoufondu@gmail.com>
2024-06-20 07:09:50 -0600

    Change data type for release_date from DATE to VARCHAR to accomodate when date is N/A

--- db/schema/movies.sql
+++ db/schema/movies.sql
@@ -3,7 +3,7 @@ CREATE TABLE movies (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   movie_title VARCHAR(255) NOT NULL,
-  release_date DATE NOT NULL,
+  release_date VARCHAR(255) NOT NULL,
   rating VARCHAR(255) NOT NULL DEFAULT '0',
   genre VARCHAR(255) NOT NULL,
   imdb_score REAL NOT NULL DEFAULT '0',
