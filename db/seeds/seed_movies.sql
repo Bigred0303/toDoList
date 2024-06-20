986b1ca3f3c19ed2ec11262a2ed69ca4b8d27671
Somto Ufondu <somtoufondu@gmail.com>
2024-06-19 23:19:04 -0600

    Update the seed to match the schema

--- db/seeds/seed_movies.sql
+++ db/seeds/seed_movies.sql
@@ -1,8 +1,8 @@
-INSERT INTO movies (name, release_date, rating, genre, imdb_score, user_id) VALUES
-('Movie1','2020-01-01', 'PG-13', 'Drama', 8, 1),
-('Movie2','2019-06-15', 'R', 'Thriller', 7, 2),
-('Movie3','2021-03-22', 'G', 'Comedy', 6, 3),
-('Movie4','2018-11-10', 'PG', 'Action', 9, 4),
-('Movie5','2022-05-30', 'NR', 'Sci-Fi', 8, 1);
+INSERT INTO movies (name, movie_title, release_date, rating, genre, imdb_score, user_id) VALUES
+('Movie1', 'Dr. Strange', '2020-01-01', 'PG-13', 'Drama', 8, 1),
+('Movie2', 'Surgeon Supreme', '2019-06-15', 'R', 'Thriller', 7, 2),
+('Movie3', 'Rainfall', '2021-03-22', 'G', 'Comedy', 6, 3),
+('Movie4', 'London Has Fallen', '2018-11-10', 'PG', 'Action', 9, 4),
+('Movie5', 'Air Force One', '2022-05-30', 'NR', 'Sci-Fi', 8, 1);
 
 -- Added name to every table so we have a value to change the name of the Task, there is a dummy value insert for it now
