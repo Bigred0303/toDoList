955fc9757f62e0783cacf798312056fc1dcf7156
Somto Ufondu <somtoufondu@gmail.com>
2024-06-19 22:53:12 -0600

    Update seed with current column

--- db/seeds/seed_foods.sql
+++ db/seeds/seed_foods.sql
@@ -1,8 +1,8 @@
-INSERT INTO restaurants (name, review_count, rating, phone_number, website_url, address, category, user_id) VALUES
-('food1', 150, 4, 1234567890, 'https://example.com/food1', '123 Main St, Anytown, USA', 'Italian', 1),
-('Chunqing Chicken', 200, 5, 2345678901, 'https://example.com/food2', '456 Elm St, Othertown, USA', 'Chinese', 2),
-('Taco Place', 100, 3, 3456789012, 'https://example.com/food3', '789 Oak St, Anycity, USA', 'Mexican', 3),
-('Sushi Place', 250, 5, 4567890123, 'https://example.com/food4', '101 Pine St, Somecity, USA', 'Japanese', 4),
-('Patoors Curry', 300, 4, 5678901234, 'https://example.com/food5', '202 Maple St, Thiscity, USA', 'Indian', 1);
+INSERT INTO restaurants (name, restaurant_name, review_count, rating, phone_number, website_url, address, category, user_id) VALUES
+('food1', 'KFC', 150, 4, 1234567890, 'https://example.com/food1', '123 Main St, Anytown, USA', 'Italian', 1),
+('food2', 'Chunqing Chicken', 200, 5, 2345678901, 'https://example.com/food2', '456 Elm St, Othertown, USA', 'Chinese', 2),
+('food3', 'Taco Place', 100, 3, 3456789012, 'https://example.com/food3', '789 Oak St, Anycity, USA', 'Mexican', 3),
+('food4', 'Sushi Place', 250, 5, 4567890123, 'https://example.com/food4', '101 Pine St, Somecity, USA', 'Japanese', 4),
+('food5', 'Patoors Curry', 300, 4, 5678901234, 'https://example.com/food5', '202 Maple St, Thiscity, USA', 'Indian', 1);
 
 -- Added name to every table so we have a value to change the name of the Task, there is a dummy value insert for it now
