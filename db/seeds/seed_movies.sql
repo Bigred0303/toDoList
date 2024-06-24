a8538757bead51f9a740367d89dcaab87c7772de
Somto Ufondu <somtoufondu@gmail.com>
2024-06-24 07:44:19 -0600

    Add positions to the seeds

--- db/seeds/seed_movies.sql
+++ db/seeds/seed_movies.sql
@@ -1,8 +1,8 @@
-INSERT INTO movies (name, movie_title, release_date, rating, genre, imdb_score, user_id) VALUES
-('Movie1', 'Dr. Strange', '2020-01-01', 'PG-13', 'Drama', 8, 1),
-('Movie2', 'Surgeon Supreme', '2019-06-15', 'R', 'Thriller', 7, 2),
-('Movie3', 'Rainfall', '2021-03-22', 'G', 'Comedy', 6, 3),
-('Movie4', 'London Has Fallen', '2018-11-10', 'PG', 'Action', 9, 4),
-('Movie5', 'Air Force One', '2022-05-30', 'NR', 'Sci-Fi', 8, 1);
+INSERT INTO movies (position, name, movie_title, release_date, rating, genre, imdb_score, user_id) VALUES
+(1, 'Movie1', 'Dr. Strange', '2020-01-01', 'PG-13', 'Drama', 8, 1),
+(2, 'Movie2', 'Surgeon Supreme', '2019-06-15', 'R', 'Thriller', 7, 2),
+(3, 'Movie3', 'Rainfall', '2021-03-22', 'G', 'Comedy', 6, 3),
+(4, 'Movie4', 'London Has Fallen', '2018-11-10', 'PG', 'Action', 9, 4),
+(5, 'Movie5', 'Air Force One', '2022-05-30', 'NR', 'Sci-Fi', 8, 1);
 
 -- Added name to every table so we have a value to change the name of the Task, there is a dummy value insert for it now
