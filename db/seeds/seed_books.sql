6417bb106dad6ff06e28dfdc74cfa6ef9bf5e302
Somto Ufondu <somtoufondu@gmail.com>
2024-06-24 07:47:03 -0600

    Correct the typos in the seed_books.sql file

--- db/seeds/seed_books.sql
+++ db/seeds/seed_books.sql
@@ -1,8 +1,8 @@
 INSERT INTO books (position, name, title, author, publish_date, page_count, purchase_link, price, language, user_id) VALUES
 (1, 'Book1', 'To Kill a Mockingbird', 'Harper Lee', '1960-07-11', 281, 'https://example.com/to-kill-a-mockingbird', 15, 'en', 1),
 (2, 'Book2', '1984', 'George Orwell', '1949-06-08', 328, 'https://example.com/1984', 20, 'en', 2),
-(3. 'Book3', 'Pride and Prejudice', 'Jane Austen', '1813-01-28', 279, 'https://example.com/pride-and-prejudice', 10, 'en', 3),
-(4. 'Book4', 'The Great Gatsby', 'F. Scott Fitzgerald', '1925-04-10', 180, 'https://example.com/the-great-gatsby', 18, 'en', 4),
-(5. 'Book5', 'Moby-Dick', 'Herman Melville', '1851-10-18', 635, 'https://example.com/moby-dick', 25, 'en', 1);
+(3, 'Book3', 'Pride and Prejudice', 'Jane Austen', '1813-01-28', 279, 'https://example.com/pride-and-prejudice', 10, 'en', 3),
+(4, 'Book4', 'The Great Gatsby', 'F. Scott Fitzgerald', '1925-04-10', 180, 'https://example.com/the-great-gatsby', 18, 'en', 4),
+(5, 'Book5', 'Moby-Dick', 'Herman Melville', '1851-10-18', 635, 'https://example.com/moby-dick', 25, 'en', 1);
 
 -- Added name to every table so we have a value to change the name of the Task, there is a dummy value insert for it now
